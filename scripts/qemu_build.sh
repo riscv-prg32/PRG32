828f2f2ddd997e25e25b41948908f244d8f1ce72
Simone Boscaglia <simone.boscaglia@gmail.com>
2026-06-04 11:02:20 +0200

    fix(qemu_build.sh): moved code into main function

--- scripts/qemu_build.sh
+++ scripts/qemu_build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -e
 
-idf.py -B build-qemu -D SDKCONFIG=build-qemu/sdkconfig -D SDKCONFIG_DEFAULTS=sdkconfig.defaults.qemu set-target esp32c3
-idf.py -B build-qemu -D SDKCONFIG=build-qemu/sdkconfig -D SDKCONFIG_DEFAULTS=sdkconfig.defaults.qemu qemu --graphics monitor
+main() {
+    set -e
+
+    idf.py -B build-qemu -D SDKCONFIG=build-qemu/sdkconfig -D SDKCONFIG_DEFAULTS=sdkconfig.defaults.qemu set-target esp32c3
+    idf.py -B build-qemu -D SDKCONFIG=build-qemu/sdkconfig -D SDKCONFIG_DEFAULTS=sdkconfig.defaults.qemu qemu --graphics monitor
+}
+main "$@"
