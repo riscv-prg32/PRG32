c219cf250425346201137b5f96afa2597ee6a609
Simone Boscaglia <simone.boscaglia@gmail.com>
2026-06-15 15:01:48 +0200

    fix(env): added env example file

---
+++ main/prg32_env_example.h
@@ -0,0 +1,14 @@
+/*
+Rename this file "prg32_env.h" and replace the dummy values with your own.
+This file is used to store environment variables that are not required for the
+system to build or run, but are used to automatically proivde WiFi credentials
+for convenience.
+*/
+
+#ifndef PRG32_ENV_H
+#define PRG32_ENV_H
+
+#define PRG32_WIFI_SSID "YOUR_WIFI_SSID"
+#define PRG32_WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#endif
